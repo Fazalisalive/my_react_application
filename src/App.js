5d9d9eea862058b4d249035b7c03b848b4bd23c7
Fazal Ur Rehman Azad <fazal8ciit@gmail.com>
2020-06-09 16:18:09 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       
       <h1>Hello World! It is my test react application</h1> 
-      Fazal Ur Rehman Azad @ 2
+      Fazal Ur Rehman Azad @ 22
     </div>
   );
 }
