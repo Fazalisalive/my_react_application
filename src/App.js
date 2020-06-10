9a7ace4f1665cd831454cc94ef26d8e76882605a
Fazal Ur Rehman Azad <fazal8ciit@gmail.com>
2020-06-11 01:05:18 +0500

    updated acknowlegements

--- src/App.js
+++ src/App.js
@@ -35,6 +35,8 @@ class App extends React.Component{
                 <Cards data={data}/>
                 <CountryPicker handleCountryChange={this.handleCountryChange}/>
                 <Charts data={data} country={country}/>
+                <h1>© 2020 TechSeige, <br />
+                    Designed by: Fazal Ur Rehman Azad</h1>
             </div>
         )
     }
