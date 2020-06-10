fc144424575bb487f07c47776d2b1eee9f45713a
Fazal Ur Rehman Azad <fazal8ciit@gmail.com>
2020-06-11 00:39:56 +0500

    updated to covid19worldtracker

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import React from 'react';
+import React from 'react'
+
+import { Cards, Charts, CountryPicker} from './Components'
+import styles from './App.module.css'
+import {fetchData} from './api'
+import coronaImages from './images/logo2.png'
 
 
-function App() {
-  return (
-    <div className="App">
-      
-      <h1>Hello World! It is my test react application</h1> 
-      Fazal Ur Rehman Azad
-    </div>
-  );
+class App extends React.Component{
+
+    state = {
+        data:{},
+        country: '',
+    }
+
+    handleCountryChange = async (country) => {
+
+        // fetch data
+        const fetchedData = await fetchData(country)
+
+        // set the state
+        this.setState({data: fetchedData, country: country})
+
+    }
+
+    async componentDidMount() {
+        const fetchedData = await fetchData()
+        this.setState({ data: fetchedData})
+    }
+    render(){
+        const { data, country } = this.state
+        return(
+            <div className={styles.container}>
+                <img className={styles.image} src={coronaImages} alt="COID-19"/>
+                <Cards data={data}/>
+                <CountryPicker handleCountryChange={this.handleCountryChange}/>
+                <Charts data={data} country={country}/>
+            </div>
+        )
+    }
+
 }
 
-export default App;
+export default App 
